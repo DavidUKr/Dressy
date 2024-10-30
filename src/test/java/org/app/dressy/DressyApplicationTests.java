b02c5d731d7e9da32f994701108c2cb4834ba76e
DavidUKr <davidkrinurs@gmail.com>
2024-10-30 22:17:03 +0200

    first commit

---
+++ src/test/java/org/app/dressy/DressyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.app.dressy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DressyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
