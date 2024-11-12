a68eb414a06a1af46da7b3b93444d93731d1d7ef
DavidUKr <davidkrinurs@gmail.com>
2024-11-12 07:54:03 +0200

    ImageController and app properties schema

---
+++ src/main/java/org/app/dressy/service/ImageService.java
@@ -0,0 +1,21 @@
+package org.app.dressy.service;
+
+import org.app.dressy.model.ImageDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ImageService {
+    public void saveImage(ImageDTO imageDTO) {
+    }
+
+    public ImageDTO getImageById(String id) {
+    }
+
+    public List<ImageDTO> getImagesByUserId(String userId) {
+    }
+
+    public void deleteImage(String id) {
+    }
+}
